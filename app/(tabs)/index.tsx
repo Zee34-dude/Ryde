c14ea22ec2cf236e014041a113edba96eb5283cb
zion ubesie <zionubesie@gmail.com>
2026-03-24 14:58:58 +0100

    fixed nativewind issue, imported global.css inside the layout file

---
+++ app/(tabs)/index.tsx
@@ -0,0 +1,16 @@
+
+import {Text } from 'react-native';
+import {SafeAreaView} from "react-native-safe-area-context"
+
+
+export default function HomeScreen() {
+  return (
+      <SafeAreaView className="flex-1 items-center justify-center bg-red-600">
+        <Text className="text-xl font-bold text-blue-400">
+          Welcome to Nativewind!
+        </Text>
+      </SafeAreaView>
+  );
+}
+
+
